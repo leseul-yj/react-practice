d9a4e95d3f103f4b11d57968c4fb9f1aec785ad9
leseul-yj <79645550@qq.com>
2020-08-15 23:57:48 +0800

    ticket页面

--- website/webpack.base.js
+++ website/webpack.base.js
@@ -9,113 +9,113 @@ const {
     CleanWebpackPlugin
 } = require("clean-webpack-plugin");
 const chunks = {
-    index: [path.join(__dirname, "./src/index/index.js")],
-    ticket: [path.join(__dirname, "./src/order/index.js")]
+    index: [path.join(__dirname,"./src/index/index.js")],
+    ticket: [path.join(__dirname,"./src/order/index.js")]
 };
 
 exports.default = {
     entry: chunks,
     output: {
-        path: path.resolve(__dirname, "./dist"),
+        path: path.resolve(__dirname,"./dist"),
         filename: "[name].[chunkhash].js",
         publicPath: "./"
     },
     module: {
         rules: [{
-                test: /(\.jsx|\.js)$/,
-                use: "babel-loader",
-                exclude: [/node_modules/]
+            test: /(\.jsx|\.js)$/,
+            use: "babel-loader",
+            exclude: [/node_modules/]
+        },
+        {
+            test: /\.css$/,
+            use: [{
+                loader: "style-loader"
             },
             {
-                test: /\.css$/,
-                use: [{
-                        loader: "style-loader"
-                    },
-                    {
-                        loader: "css-loader",
-                        options: {
-                            sourceMap: true
-                        }
-                    }
-                ]
-            },
-            {
-                test: /\.less$/,
-                include: [path.resolve(__dirname, "node_modules"), path.resolve(__dirname, "src/theme")],
-                use: ["style-loader", "css-loader", "less-loader"]
-            },
-            {
-                test: /\.less$/,
-                exclude: [path.resolve(__dirname, "node_modules"), path.resolve(__dirname, "src/theme")],
-                use: [{
-                        loader: "style-loader" // creates style nodes from JS strings
-                    },
-                    {
-                        loader: "css-loader", // translates CSS into CommonJS
-                        options: {
-                            camelCase: true,
-                            modules: true,
-                            importLoaders: 1,
-                            localIdentName: "[folder]__[name]__[local]__[hash:base64:5]"
-                        }
-                    },
-                    {
-                        loader: "less-loader" // compiles Less to CSS
-                    }
-                ]
-            },
-            {
-                test: /\.json$/,
-                loader: "json-loader"
-            },
-            {
-                test: /\.html$/,
-                use: {
-                    loader: "html-loader",
-                    options: {
-                        minimize: true
-                    }
+                loader: "css-loader",
+                options: {
+                    sourceMap: true
                 }
-            },
-            {
-                test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
-                use: {
-                    loader: "url-loader",
-                    options: {
-                        limit: 10000,
-                        mimetype: "application/font-woff"
-                    }
-                }
-            },
-            {
-                test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
-                use: {
-                    loader: "url-loader",
-                    options: {
-                        limit: 10000,
-                        mimetype: "application/font-woff"
-                    }
-                }
-            },
-            {
-                test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-                use: {
-                    loader: "url-loader",
-                    options: {
-                        limit: 10000,
-                        mimetype: "application/octet-stream"
-                    }
-                }
-            },
-            {
-                test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-                use: "file-loader"
             }
+            ]
+        },
+        {
+            test: /\.less$/,
+            include: [path.resolve(__dirname,"node_modules"),path.resolve(__dirname,"src/theme")],
+            use: ["style-loader","css-loader","less-loader"]
+        },
+        {
+            test: /\.less$/,
+            exclude: [path.resolve(__dirname,"node_modules"),path.resolve(__dirname,"src/theme")],
+            use: [{
+                loader: "style-loader" // creates style nodes from JS strings
+            },
+            {
+                loader: "css-loader", // translates CSS into CommonJS
+                options: {
+                    camelCase: true,
+                    modules: true,
+                    importLoaders: 1,
+                    localIdentName: "[folder]__[name]__[local]__[hash:base64:5]"
+                }
+            },
+            {
+                loader: "less-loader" // compiles Less to CSS
+            }
+            ]
+        },
+        {
+            test: /\.json$/,
+            loader: "json-loader"
+        },
+        {
+            test: /\.html$/,
+            use: {
+                loader: "html-loader",
+                options: {
+                    minimize: true
+                }
+            }
+        },
+        {
+            test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
+            use: {
+                loader: "url-loader",
+                options: {
+                    limit: 10000,
+                    mimetype: "application/font-woff"
+                }
+            }
+        },
+        {
+            test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
+            use: {
+                loader: "url-loader",
+                options: {
+                    limit: 10000,
+                    mimetype: "application/font-woff"
+                }
+            }
+        },
+        {
+            test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+            use: {
+                loader: "url-loader",
+                options: {
+                    limit: 10000,
+                    mimetype: "application/octet-stream"
+                }
+            }
+        },
+        {
+            test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+            use: "file-loader"
+        }
         ]
     },
     resolve: {
-        extensions: [".js", ".jsx", ".json", ".css", ".less"],
-        modules: [__dirname, "node_modules", path.resolve(__dirname, "./src/index"), path.resolve(__dirname, "./src/ticket")]
+        extensions: [".js",".jsx",".json",".css",".less"],
+        modules: [__dirname,"node_modules",path.resolve(__dirname,"./src/index"),path.resolve(__dirname,"./src/ticket")]
     },
     plugins: [
         new CleanWebpackPlugin(),
@@ -135,7 +135,7 @@ exports.default = {
             hash: true,
             title: "ticket",
             filename: "./ticket.html",
-            template: "./public/ticket.html",
+            template: "./public/ticket.ejs",
             alwaysWriteToDisk: true,
             chunks: ["ticket"],
             chunksSortMode: "manual",
